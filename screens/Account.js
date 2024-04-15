6840115fca2d7f55b0a452e8232027ad28a53df3
Anatude1234 <antti.korpua1@gmail.com>
2024-04-15 11:53:58 +0300

    FavoriteRecipes

--- screens/Account.js
+++ screens/Account.js
@@ -1,7 +1,6 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Profileinfo from '../components/Profileinfo'
-import EditProfile from '../components/EditProfile'
 import { auth } from 'firebase/auth'
 import { useAuth, signOut } from '../context/useAuth'
 import { getAuth } from 'firebase/auth'
@@ -9,6 +8,7 @@ import OwnRecipes from '../components/OwnRecipes'
 import UserCard from '../components/EditProfile/UserCard'
 import UserInformationCard from '../components/EditProfile/UserInformationCard'
 import ChangePassword from '../components/EditProfile/ChangePassword'
+import FavoriteRecipes from '../components/FavoriteRecipes';
 
 
 
@@ -23,7 +23,8 @@ export default function Account() {
     <View>
       <UserCard/>
       <UserInformationCard/>
-      <ChangePassword/>
+      <ChangePassword/>  
+        <FavoriteRecipes />
     </View>
   )
 } 

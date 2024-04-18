0311e542cd57480b8877d29f7afd4055a801f924
MiikkaJanesaro <122359891+MiikkaJanesaro@users.noreply.github.com>
2024-04-18 13:01:26 +0300

    juu

--- components/TheMealDB/SearchBy.js
+++ components/TheMealDB/SearchBy.js
@@ -15,4 +15,18 @@ const fetchMealById = async (id) => {
     }
 }
 
-export { fetchMealById }
+const fetchRandomMeal = async () =>{
+
+
+    try {
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+        const data = await response.json()
+        const randomMeal = (data.meals ? data.meals[0] : null)
+        console.log(randomMeal)
+        return randomMeal
+    } catch (error) {
+        console.log("Failed to fetch random meals", error);
+    }
+}
+
+export { fetchMealById, fetchRandomMeal }

1243140c6ae18cb06df99a462357a3fea27a9a12
Ville Niemi <vipeni35@gmail.com>
2024-04-23 15:50:13 +0300

    Login asetteluja

--- screens/Login.js
+++ screens/Login.js
@@ -1,15 +1,14 @@
 import { useState, useEffect, useContext } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 import { Button, TextInput } from 'react-native-paper';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { getAuth, auth, signInWithEmailAndPassword, signOut, } from '../firebase/config';
-import { firestore, collection, addDoc, serverTimestamp } from '../firebase/config';
 import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../context/useAuth';
 import { AuthContext } from '../context/CreateAuthContext';
 import Styles from '../Styles';
 import { useTheme } from '../context/useTheme';
-import Stacknav from '../components/Stacknav';
-import TopBarMenu from '../components/TopBar/LoginButton';
+
 
 
 export default function Login() {
@@ -17,6 +16,7 @@ export default function Login() {
 	const [password, setPassword] = useState('');
 	const [logged, setLogged] = useState(false);
 	const [errorMessage, setErrorMessage] = useState('');
+	const [errorTrue, setErrorTrue] = useState(false);
 	const { setUser } = useAuth()
 	const navigation = useNavigation()
 	const isDarkMode = useTheme();
@@ -31,8 +31,9 @@ export default function Login() {
 				navigation.reset({ index: 0, routes: [{ name: 'Home' }] })
 			})
 			.catch((error) => {
-				if (error.code === 'auth/wrong-password' || error.code === 'auth/user-not-found') {
-					console.log('Invalid credentials');
+				setErrorTrue(true);
+				if (error.code === 'auth/wrong-password' || error.code === 'auth/user-not-found' || error.code === 'auth/invalid-credential') {
+					console.log('Invalid credentialssss');
 					setErrorMessage('Invalid credentials!');
 				} else if (error.code === 'auth/too-many-requests') {
 					console.log('Too many attempts to login');
@@ -45,10 +46,12 @@ export default function Login() {
 
 	return (
 		<View style={[styles.inputView]}>
+			<Text style={styles.header}>Login here</Text>
 			<TextInput
 				style={styles.TextInput}
 				label="Username"
 				mode='outlined'
+				error={errorTrue}
 				secureTextEntry={false}
 				onChangeText={(username) => setUsername(username)}
 			/>
@@ -56,11 +59,15 @@ export default function Login() {
 				style={styles.TextInput}
 				label="Password"
 				mode='outlined'
+				error={errorTrue}
 				secureTextEntry={true}
 				onChangeText={(password) => setPassword(password)}
 			/>
-			<Text>{errorMessage}</Text>
-			<Button style={styles.loginButton} onPress={login}>Login</Button>
+			<Text style={styles.errorText}>{errorMessage}</Text>
+			<Pressable style={styles.loginButton} onPress={login}>
+				<MaterialCommunityIcons name="check" color={'black'} size={30} />
+				<Text style={styles.buttonText}>Login</Text>
+        	</Pressable>
 		</View>
 	);
 }
@@ -87,7 +94,14 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		alignItems: 'center',
-	  },
+	},
+		header: {
+		fontSize: 24,
+		paddingTop: 24,
+		paddingBottom: 24,
+		marginHorizontal: 12,
+		textAlign: 'center'
+	},
 	inputView: {
 		height: 1000,
 		marginBottom: 20,
@@ -102,9 +116,29 @@ const styles = StyleSheet.create({
 		padding: 10,
 		borderRadius: 3,
 	},
-	loginButton: {
-		width: 220,
-		margin: 10,
+	loginText: {
+		marginLeft: 10,
+		fontSize: 16,
+		lineHeight: 21,
+		fontWeight: 'bold',
+		letterSpacing: 0.25,
+		color: 'white',
+	},
+	loginButton:{
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'center',
+		width: 300,
+		paddingVertical: 12,
+		paddingHorizontal: 32,
+		marginHorizontal: 15,
+		marginVertical: 8,
+		borderRadius: 4,
+		elevation: 3,
+		backgroundColor: 'green'
+	},
+	errorText: {
+		fontSize: 15,
 	},
 });
 
